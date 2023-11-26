1124707dbfd248549d1def147040106a4a03e530
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-11-26 01:14:15 +0100

    and again, yet another task

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import types
+
+
+def print_hidden_names(module):
+    names = dir(module)
+    filtered_names = [name for name in names if not name.startwith("__")]
+
+    sorted_names = sorted(filtered_names)
+    for name in sorted_names:
+        print(name)
+
+
+if __name__ == "__main__":
+    # Load the compiled module
+    with open("hidden_4.pyc", "rb") as file:
+        code = file.read()
+        module = types.ModuleType("hidden_4")
+        exec(compile(code, "hidden_4.pyc", "exec"), module.__dict__)
+
+        # Print the hidden names
+        print_hidden_names(module)
