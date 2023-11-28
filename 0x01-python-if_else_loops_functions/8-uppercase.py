3acdfc22bfb206612861a6a7313d2917f21c562b
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-11-28 19:34:04 +0100

    Changed the str.upper() function to my own derived method

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    str = str.upper()
+    str = list(str)
+    for i in range(len(str)):
+        if 97 <= ord(str[i]) <= 123:
+            str[i] = chr(ord(str[i]) - ord('a') + ord('A'))
+        else:
+            str[i] = str[i]
+    str = "".join(i for i in str)
     print("{}".format(str))
