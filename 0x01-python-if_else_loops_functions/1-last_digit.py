732f6a80a85312a8e7d8d532ddf4175e71ade445
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-11-28 18:25:22 +0100

    Pycodestyle validation format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,7 +11,8 @@ else:
     lastdigit = number % 10
 
 if lastdigit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, lastdigit))
+    print("Last digit of {} is {} and is greater than \
+5".format(number, lastdigit))
 elif lastdigit == 0:
     print("Last digit of {} is {} and is 0".format(number, lastdigit))
 else:
