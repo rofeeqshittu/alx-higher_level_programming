4aa9c21da3e48a24d090ad7e8e5df484d4e13602
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-12-20 23:24:09 +0100

    getter setter added

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+A module Square contain a Square class.
+"""
+
+
+class Square:
+    """ Square class that returns area of square. """
+
+    def __init__(self, size=0):
+        """Initialization method with optional size."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method to retrieve the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method to set size."""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """Area of square."""
+
+        return self.__size ** 2
