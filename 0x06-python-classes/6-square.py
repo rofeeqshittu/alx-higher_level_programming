5f455237220dad3292b1262bda1f2b2624c96384
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-12-21 00:53:01 +0100

    changed method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,7 +9,12 @@ class Square:
     """ Square class that returns area of square. """
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialization method with position & optional size."""
+        """Initialize with position & optional size.
+
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
         self.__size = size
         self.__position = position
 
@@ -35,10 +40,10 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        x, y = value
-        if not isinstance(x, int) or not isinstance(y, int) or x < 0 or y < 0:
+        if (not isinstance(value, tuple) or 
+            len(value) != 2 or
+            not all(isinstance(num, int) for num in value) or
+            not all(num >= 0 for num in value)):
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
@@ -50,8 +55,10 @@ class Square:
         """Prints in stdout the square with the character #."""
         if self.__size == 0:
             print()
-        else:
-            for _ in range(self.position[1]):
-                print()
-            for _ in range(self.__size):
-                print(' ' * self.__position[0] + '#' * self.__size)
+            return
+
+        [print("") for i in range(self.__position[1])]
+        for _ in range(self.__size):
+            [print(" ", end="") for j in range(self.__position[0])]
+            [print("#", end="") for k in range(self.__size)]
+            print("")
--- 0x06-python-classes/maintest.py
+++ 0x06-python-classes/maintest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
+Square = __import__('6-square').Square
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+try:
+    my_square = Square(3, ("Position",))
+except Exception as e:
+    print(e)
