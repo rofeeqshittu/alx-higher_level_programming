99ed97575295fc7a48c29b50909e06e17ffdedd4
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-12-27 03:45:42 +0100

    fixed pep error

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,7 +5,8 @@
 
 
 def text_indentation(text):
-    """ Prints a text with 2 new lines after each of these characters: ., ? and : """
+    """ Prints a text with 2 new lines after each of these characters:
+        ., ? and : """
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
