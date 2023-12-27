b4845daffbd8fe83304953385940da63d39897de
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-12-27 03:38:26 +0100

    indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Contains a text_indentation(text) function that prints something
+"""
+
+
+def text_indentation(text):
+    """ Prints a text with 2 new lines after each of these characters: ., ? and : """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    punctuation = ".?:"
+    buffer = ""
+
+    for char in text:
+        buffer += char
+        if char in punctuation:
+            print(buffer.strip())
+            print()
+            buffer = ""
+
+    if buffer:
+        print(buffer.strip())
