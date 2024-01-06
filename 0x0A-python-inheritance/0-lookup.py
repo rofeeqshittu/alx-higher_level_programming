47d8c18eef581805b48b2cb1dff1c71436c3cb4c
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-01-06 01:53:22 +0100

    first commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Module containing lookup() function
+"""
+
+
+def lookup(obj):
+    """ Returns the list of available attributes and methods of an object """
+
+    return [attr for attr in dir(obj) if not callable(getattr(obj, attr)) or hasattr(obj, attr)]
