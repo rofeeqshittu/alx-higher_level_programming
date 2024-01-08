ff317111693bf6843ccf64af1150b042d1336e76
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-01-08 23:57:48 +0100

    add attr using setattr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -7,15 +7,6 @@
 def add_attribute(obj, attribute, value):
     """ Add attribute to an object if it's possible """
 
-    if not hasattr(obj, "__dict__") and not hasattr(obj, "__slots__"):
+    if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
-
-    if hasattr(obj, "__slots__"):
-        slots = getattr(obj, "__slots__")
-        if not isinstance(slots, (list, tuple)):
-            slots = [slots]
-
-            if attribute not in slots:
-                raise TypeError("can't add new attribute")
-
     setattr(obj, attribute, value)
