817f62298e846d5541a686cb1ea5506e3b33b40c
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-01-08 23:50:38 +0100

    re-used the super() in subclass!

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -39,5 +39,10 @@ class Square(Rectangle):
     """ Class inherit from Rectangle """
 
     def __init__(self, size):
-        super().__init__(size, size)
         self.__size = self.integer_validator("size", size)
+
+     def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
