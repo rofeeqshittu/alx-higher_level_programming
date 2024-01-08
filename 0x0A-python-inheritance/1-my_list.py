879619f6c6c01f1c2436da349f7e092d9f8573c2
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-01-08 23:35:12 +0100

    refix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,10 @@
 class MyList(list):
     """ This class inherits from class list """
 
+    def __init__(self):
+        " initializes the object """
+        super().__init()
+
     def print_sorted(self):
         """ Prints a sorted list given """
         print(sorted(self))
