061d405af49cade8f93a2f74684ab9d553d31920
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-01-09 22:11:50 +0100

    fixed pycodestyle error - Student to disk and reload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -21,6 +21,6 @@ class Student:
 
     def reload_from_json(self, json):
         """ Replaces all attributes of the Student instance """
-        
+
         for key, value in json.items():
             setattr(self, key, value)
