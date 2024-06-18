4c1a42e7d09916826e061808d9d1cd8dde2ebbb3
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-06-18 15:00:25 +0100

    try

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT * FROM information_schema.columns WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+SELECT table_name, column_name, column_type, is_nullable, column_default
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
