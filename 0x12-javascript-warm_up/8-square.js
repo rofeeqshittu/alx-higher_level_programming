358e8af9eb6f47f070203de9bfceac5f1218ab4d
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-06-20 00:22:15 +0100

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,7 +7,7 @@ if (process.argv.length <= 2 || isNaN(num)) {
   console.log('Missing size');
 } else {
   if (!isNaN(num) && num > 1) {
-    let i = num;
+    const i = num;
     while (num !== 0) {
       console.log('x'.repeat(i));
       num--;
