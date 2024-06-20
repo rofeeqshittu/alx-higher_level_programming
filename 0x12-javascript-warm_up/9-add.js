3536b7753aa921d60fe116834b3b44bee57dd7d6
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-06-20 01:17:26 +0100

    re Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,12 +3,16 @@
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
+function add (a, b) {
+  return (Number(a) + Number(b));
+}
+
 if (process.argv.length < 4) {
   console.log('NaN');
 } else {
   if (arg1 === undefined || isNaN(arg1) || arg2 === undefined || isNaN(arg2)) {
     console.log('NaN');
   } else {
-    console.log(Number(arg1) + Number(arg2));
+    console.log(add(arg1, arg2));
   }
 }
