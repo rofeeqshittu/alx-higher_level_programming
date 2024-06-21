b17bfccad9d3e3b93ff0abb1e6452decdd6f05b4
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-06-21 02:16:44 +0100

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@ exports.nbOccurences = function (list, searchElement) {
   let nOccurrences = 0;
   for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
-      nOccurrences++;
+      nOccurrences = nOccurrences + 1;
     }
   }
   return nOccurrences;
