d7edca277704d8e4e75d386b6b8c915591159b5d
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-06-25 22:37:08 +0100

    filter all states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,39 +1,18 @@
 #!/usr/bin/python3
-"""
-    List  all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
-"""
-import sys
+""" List all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-   # Connect to MySQL
-   try:
-       db = MySQLdb.connect(
-               host="localhost", port=3306, user=username,
-               passwd=password, db=database
-               )
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL:", e)
-        sys.exit(1)
-
-    # Prepare a cursor object using cursor() method
-    cursor = db.cursor()
-
-    # Execute SQL query
-    try:
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-    except MySQLdb.Error as e:
-        print("Error executing MySQL query:", e)
-        sys.exit(1)
-
-    # Disconnect from server
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+    LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
