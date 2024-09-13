5e1256276f87939467dae2297e8b55f36e26ebf8
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-09-13 21:19:25 +0100

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@ CREATE USER IF NOT EXITS ' user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Apply the changes
-FLUSH PRIVILEGES
+FLUSH PRIVILEGES;
