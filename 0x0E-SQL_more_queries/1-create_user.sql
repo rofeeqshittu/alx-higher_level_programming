40885f759cf423222430c67468e1e9781998bbd4
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-09-13 21:25:11 +0100

    removed flush privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- A script that creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXITS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'; --  WITH GRANT OPTION;
 
 -- Apply the changes
-FLUSH PRIVILEGES;
+-- FLUSH PRIVILEGES;
