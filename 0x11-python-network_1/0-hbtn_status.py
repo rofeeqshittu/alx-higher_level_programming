743c7ce058feb4672b9c1bbea003bd294bad47b8
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-09-26 19:23:32 +0100

    0-hbtn_status.py doc addded

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+"""
+This script fetches the status of the ALX intranet and prints the 
+response body including the type, content, and utf8 content. 
+It demonstrates the use of urllib to make HTTP requests in Python.
+"""
 import urllib.request
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    
-    # Fetching the URL
+def fetch_status(url):
+    """Fetch the status from the given URL and print the response."""
     with urllib.request.urlopen(url) as response:
         body = response.read()
-        
-        # Displaying the response
         print("Body response:")
         print(f"    - type: {type(body)}")
         print(f"    - content: {body}")
         print(f"    - utf8 content: {body.decode('utf-8')}")
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    fetch_status(url)
 
